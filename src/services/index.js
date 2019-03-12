653a7a70a66734b0f9880f3c4194105230bf64e8
abhay-srivastav <abhay@Administrators-MacBook-Air-4.local>
2019-03-12 17:33:23 +0530

    endpoint change

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export function getCoursesInfo(ids) {
-	let url = 'http://ec2-18-219-109-248.us-east-2.compute.amazonaws.com:8080/getCourseInfo';
+	let url = 'http://ec2-18-219-109-248.us-east-2.compute.amazonaws.com:8080/getCourseInfoWeb';
 	if(ids){
 		url+= ('?courseIds='+ids)
 	}
